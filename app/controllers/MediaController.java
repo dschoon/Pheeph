79cb01a4985f9ae0ed39bb10376aa101043478e6
Dan Schoonmaker <daniel.schoonmaker@lithium.com>
2014-09-03 20:00:30 -0500

    Adding more game details around top plays

--- app/controllers/MediaController.java
+++ app/controllers/MediaController.java
@@ -3,10 +3,14 @@ package controllers;
 import controllers.securesocial.SecureSocial;
 import models.Game;
 import models.Media;
+import models.Team;
 import models.User;
 import securesocial.provider.SocialUser;
 import play.mvc.*;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -21,13 +25,22 @@ public class MediaController extends Controller  {
         String title = "Recent Videos";
         SocialUser user = SecureSocial.getCurrentUser();
         User userModel = User.find("byEmail", user.email).first();
-        List<List<Game>> weeks = GameController.getAllGamesForAllWeeks(16);
         Media video = getRecentMedia(RECENT_COUNT).get(video_index);
         int results = getRecentMedia(RECENT_COUNT).size();
 
+        Game game = Game.getGameById(video.game_id);
+        Team homeTeam = Team.getTeamById(game.home_team_id);
+        Team awayTeam = Team.getTeamById(game.away_team_id);
+        User homeUser = User.getUserById(homeTeam.user_id);
+        User awayUser = User.getUserById(awayTeam.user_id);
+
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date gameDate = new Date(game.getMatchDate());
+        String gameTime = df.format(gameDate);
+
         renderArgs.put("userTeam",TeamController.getTeamByUserId(userModel.id));
 
-        render("Application/media.html", title, weeks, video, video_index, results);
+        render("Application/media.html", title, game, video, video_index, results, homeTeam, awayTeam, homeUser, awayUser, gameTime);
     }
 
     public static List<Media> getMediaByGameId(long game_id) {
